4ca9020aaf2526a6ba63a35b978c0a4de7df0b17
androsik95 <gerasimukandrej@gmail.com>
2023-07-29 09:25:18 +0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 
 
 const drawUsers = (data,employeeTable) => {
-  employeeTable.innerHTML = `<thead>
-        <tr>
-          <td> </td>
-          <td>Name</td>
-          <td>Surname</td>
-          <td>Salary</td>
-          <td> work start</td>
-        </tr>
-      </thead>
-      `;
-
+  employeeTable.innerHTML = ``;
 
 
   data.forEach((item) => {
@@ -53,6 +43,65 @@ const submitHandler = () => {
   };
   return person;
 };
+const toggleModal = (data, event) => {
+  const modalWrapper = document.querySelector(".modalWrapper");
+  modalWrapper.classList.toggle("toggleModal");
+
+  const editBtn = document.querySelector("#editBtn");
+  editBtn.removeEventListener("click", editBtnHandler);
+  editBtn.addEventListener("click", () => editBtnHandler(data, event));
+};
+
+const editBtnHandler = (data, event) => {
+  const employeeTable = document.querySelector("#employeeTable");
+  const cardIndex = data.findIndex((item) => item.id === event.id);
+
+  data.splice(cardIndex, 1, {
+    id: event.id,
+    name: editName.value,
+    surname: editSurname.value,
+    salary: editSalary.value,
+    wordDate: editWordDate.value,
+  });
+
+  toggleModal(data, event);
+  drawUsers(data, employeeTable);
+};
+
+const getSelectedPersonId = (employeeTable) => {
+  const checkboxes = employeeTable.querySelectorAll(
+    ".users input[type='checkbox']:checked"
+  );
+  let selectedCardId = null;
+
+  checkboxes.forEach((checkbox) => {
+    if (checkboxes) {
+      selectedCardId = +checkbox.closest(".users").id;
+    }
+  });
+
+  return selectedCardId;
+};
+
+const deletePersons = (data, employeeTable) => {
+  const checkboxes = employeeTable.querySelectorAll(
+    "tbody .users input[type='checkbox']"
+  );
+
+  const rowsToDelete = [];
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      const row = checkbox.closest(".users");
+      rowsToDelete.push(row);
+    }
+  });
+
+  rowsToDelete.forEach((row) => {
+    const id = row.getAttribute("id");
+    data = data.filter((person) => person.id !== parseInt(id));
+  });
+
+  return data;}
 
 
 const init = () => {
@@ -60,7 +109,10 @@ const init = () => {
   const submit = document.querySelector("#submit");
   const employeeTable = document.querySelector("#employeeTable");
   const deleteAll = document.querySelector("#deleteAll");
-
+  const deleteBtn = document.querySelector("#deleteBtn");
+  const closeBtn = document.querySelector("#closeBtn");
+  const edit = document.querySelector("#edit");
+  
   submit.addEventListener("click", (event) => {
     event.preventDefault();
     const person = submitHandler();
@@ -74,6 +126,32 @@ const init = () => {
   deleteAll.addEventListener("click", () => {
     data = [];
     drawUsers(data, employeeTable);
+  });
+  deleteBtn.addEventListener("click", () => {
+    data = deletePersons(data, employeeTable);
+    drawUsers(data, employeeTable);
+  });
+
+  edit.addEventListener("click", () => {
+    const cardId = getSelectedPersonId(employeeTable);
+    const event = data.find((item) => item.id === cardId);
+
+    toggleModal(data, event);
+
+    editName.value = event.name;
+    editSurname.value = event.surname;
+    editSalary.value = event.salary;
+    editWordDate.value = event.wordDate;
+  });
+
+  employeeTable.addEventListener("click", (event) => {
+    if (event.target.classList.contains("edit")) {
+      editHandler(event, data);
+    }
+  });
+
+  closeBtn.addEventListener("click", () => {
+    toggleModal();
   });
 };
 
